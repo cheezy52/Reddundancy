f262288ada23b4610b2f0e92ab56c1cb06c865a6
Benjamin Smith <bsmith.eng@gmail.com>
2014-03-19 11:29:19 -0700

    initial commit

---
+++ app/assets/javascripts/seddit.js
@@ -0,0 +1,13 @@
+// window.Seddit = {
+//   Models: {},
+//   Collections: {},
+//   Views: {},
+//   Routers: {},
+//   initialize: function() {
+//     alert('Hello from Backbone!');
+//   }
+// };
+//
+// $(document).ready(function(){
+//   Seddit.initialize();
+// });
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seddit::Application.config.secret_key_base = '49fc3892ce96514f54a8b535e5a95b7edb15cfadfbfeeb8dcba447d647843485956814bfa60378006358a0ff013d6353fa6a42840040f0a9d98da060a9cdd44e'
